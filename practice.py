2eb3ae482f7c33bc2bc78f6e7fd1d0601e1e14dc
kimdayeon <hamoey@naver.com>
2022-11-09 00:19:56 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,20 @@
+print(5)
+print(-10)
+print(3.14)
+print(5 + 3)
+
+# boolean
+print(5 > 10) 
+print(not 5 > 10) 
+print(not True)
+
+# 변수
+# 애완동물을 소개해 주세요!
+animal = "강아지"
+name = "연탄"
+age = 4
+is_adult = age >= 3
+
+print("우리집 " + animal + "의 이름은 " + name + " 이예요!")
+print(name + "는 " + str(age) + "살이예요!")
+print(name + "는 어른일까요? " + str(is_adult))
